28267868fac3fe48d7d85e3795d3303c045c74ce
Robert Schneider <Robert.Schneider3@student.kit.edu>
2012-06-28 22:00:32 +0200

    Slightly enhanced map code example

--- slides/cpp-code/map-usage.cpp
+++ slides/cpp-code/map-usage.cpp
@@ -1,5 +1,8 @@
 #include <map>
+typedef std::map < int, bar_t > Barmap_t;
+Barmap_t barmap;                 // create a map mapping int -> bar_t
+barmap[23] = bar_t("foo");       // insert a bar_t with key 23
 
-std::map<int, bar_t> barmap; // create a map mapping int -> bar_t
-barmap[23] = bar_t("foo"); // insert a bar_t with key 23
-barmap.insert(pair<int, bar_t>(42, "wuppdi");
+typedef Barmap_t::const_iterator it_t;
+it_t found = barmap.find(23);   // search for an element using a key
+if(barmap.end() == found) { /* no element with this key */ }
