9b85c7ca991c6399d263cd7b3fe564a7823c830c
Robert Schneider <Robert.Schneider3@student.kit.edu>
2012-06-29 01:54:36 +0200

    Adds more about templates

---
+++ slides/cpp-code/non-type-template-parameters.cpp
@@ -0,0 +1,13 @@
+﻿/*$ int $*/
+template < typename T_SomeType, int t_someInt >
+struct MyStruct
+{
+	T_SomeType arr[t_someInt];
+};
+
+/*$ instantiation $*/
+MyStruct < double, 42 >    myArr;
+
+int myInt;
+std::cin >> myInt;
+MyStruct < bool, myInt >   myArr2;	// FEHLER!
---
+++ slides/cpp-code/type-deduction.cpp
@@ -0,0 +1,8 @@
+﻿/*$ begin $*/
+template < typename T >
+void foo( T p )
+{ std::cout << p; }
+
+foo < int > ( 42 );
+/*$ deduction $*/
+foo(42);
