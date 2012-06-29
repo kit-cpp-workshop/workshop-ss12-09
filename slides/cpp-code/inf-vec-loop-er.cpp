9fe1043c9ad7088b731712399d420c4a5ad1a657
Robert Schneider <Robert.Schneider3@student.kit.edu>
2012-06-29 03:07:16 +0200

    Adds notes to iterators

---
+++ slides/cpp-code/inf-vec-loop-er.cpp
@@ -0,0 +1,18 @@
+﻿/*$ begin $*/
+std::vector < int > vec = { 0, 1, 2, 3 };
+
+for(It i = vec.begin(); i != vec.end(); ++i) {
+	if( 1 == *i ) {
+/*$no-assign$*/			vec.erase(i);	// schlechte Idee!
+/*$assign$*/		i = vec.erase(i);	// genauso schlecht!
+	}
+}
+/*$ while $*/
+It i = vec.begin();
+while(i != vec.end()) {
+	if( 1 == *i ) { i = vec.erase(); continue; }
+	++it;
+}
+/*$ remove_if $*/
+#include <algorithm>
+std::remove_if( vec.begin(), vec.end(), 1 );
